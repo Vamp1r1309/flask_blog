9f434e575da94443e614c82f0760a78b4ffb1926
Vamp1r1309 <shlikov.vadim@gmail.com>
2022-06-10 23:04:30 +0300

    Added blog pages

--- run.py
+++ run.py
@@ -7,6 +7,24 @@ app = Flask(__name__)
 def home():
     return render_template('index.html', title='Главная')
 
+@app.route('/html')
+def html_page():
+    return render_template('html_page.html')
+@app.route('/css')
+def css_page():
+    return render_template('css_page.html')
+@app.route('/python')
+def python_page():
+    return render_template('python_page.html')
+@app.route('/js')
+def js_page():
+    return render_template('js_page.html')
+@app.route('/flask')
+def flask_page():
+    return render_template('flask_page.html')
+@app.route('/django')
+def django_page():
+    return render_template('django_page.html')
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True, port=5656)
